c627078e5c481ebc53de57644df30c5bcd0e7f58
lunazkoe <lunazkoe@gmail.com>
2026-04-17 01:25:59 +0900

    [FINISH] S05

---
+++ C03_Spring_Core_1/S02_P/src/test/java/com/hello/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,62 @@
+package com.hello.beanfind;
+
+import com.hello.AppConfig;
+import com.hello.member.MemberRepository;
+import com.hello.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로만 조회 시 같은 타입이 둘 이상 있으면, 중복 오류가 난다.")
+    void findBeanByTypeDuplicate() {
+        assertThatThrownBy(() -> ac.getBean(MemberRepository.class))
+                .isInstanceOf(NoUniqueBeanDefinitionException.class);
+    }
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByName() {
+        MemberRepository memberRepository1 = ac.getBean("memberRepository1", MemberRepository.class);
+        MemberRepository memberRepository2 = ac.getBean("memberRepository2", MemberRepository.class);
+        assertThat(memberRepository1).isNotSameAs(memberRepository2);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key);
+            System.out.println("beansOfType.get(key) = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
---
+++ C03_Spring_Core_1/S02_P/src/test/java/com/hello/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package com.hello.xml;
+
+import com.hello.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    void xmlAppContext() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
